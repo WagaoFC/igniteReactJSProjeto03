894320e1e9842ab1410450bff5066a1c2faa94d2
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-12-12 16:09:10 -0300

    adicionando componente Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+import { SumaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+    return (
+        <SummaryContainer>
+            <SumaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size={32} color='#00b37e' />
+                </header>
+                <strong>R$ 17.400,00</strong>
+            </SumaryCard>
+
+            <SumaryCard>
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size={32} color='#f75a68' />
+                </header>
+                <strong>R$ 17.400,00</strong>
+            </SumaryCard>
+
+            <SumaryCard variant='green'>
+                <header>
+                    <span>Total</span>
+                    <CurrencyDollar size={32} color='#fff' />
+                </header>
+                <strong>R$ 17.400,00</strong>
+            </SumaryCard>
+        </SummaryContainer>
+    )
+}
