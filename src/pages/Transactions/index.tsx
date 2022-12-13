4bd6043235441d977033d4587a80c78fb961d1f2
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-12-13 13:45:43 -0300

    adicionando formatacao de data e valores em dinheiro

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,7 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles"
 
@@ -22,11 +23,12 @@ export function Transactions() {
                                     <td width='50%'>{transaction.description}</td>
                                     <td>
                                         <PriceHighlight variant={transaction.type}>
-                                            {transaction.price}
+                                            {transaction.type === 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
                                         </PriceHighlight>
                                     </td>
                                     <td>{transaction.category}</td>
-                                    <td>{transaction.createdAt}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                                 </tr>
                             )
                         })}
